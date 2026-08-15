c7c9101ef3d82efb874292ee4cbfdacf21f6e7f3
mirzabd01 <khatibusba@gmail.com>
2026-08-16 01:19:58 +0600

    Added package.json

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', padding: '2rem' }}>
+      <h1>Project</h1>
+      <p>The application is running.</p>
+    </div>
+  )
+}
